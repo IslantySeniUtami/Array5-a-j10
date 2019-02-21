4e6ec72af9ec6081a5c0b93fc98a07eb0705224f
IslantySeniUtami <46374176+IslantySeniUtami@users.noreply.github.com>
2019-02-21 09:04:06 +0700

    Add files via upload

---
+++ Tugaspraktikum1.java
@@ -0,0 +1,13 @@
+public class Tugaspraktikum1 {
+    public static void main(String[] args) {
+        int bilangan[] = {1,2,3,4,5};
+        System.out.println(bilangan[0]);
+        System.out.println(bilangan[1]);
+        System.out.println(bilangan[2]);
+        System.out.println(bilangan[3]);
+        System.out.println(bilangan[4]);
+        System.out.println(bilangan[5]);
+        
+    }
+    
+}
